1c3cd1c22318399b77bfa93975a45b98234e74c1
Damon118 <damongaffney@rcsi.ie>
2021-03-27 14:28:50 +0000

    Create sendMail function in sendEmailIndex.js

---
+++ assets/js/sendEmailIndex.js
@@ -0,0 +1,16 @@
+function sendMail(consultationButton) {
+    emailjs.send("gmail", "template_ngde9d4"){
+        "phone_number": consultationButton.phone_number.value
+        "date": consultationButton.date.value
+        "time": consultationButton.time.value
+    })
+
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+        return false;
+}

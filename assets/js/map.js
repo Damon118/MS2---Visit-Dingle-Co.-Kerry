0b7efdcb806548b6814def30321810f1bcc4bda0
Damon118 <damongaffney@rcsi.ie>
2021-03-29 22:00:30 +0000

    Add locationsMap variable to map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,6 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 6,
+        zoom: 7,
         center: {
             lat: 52.14533445,
             lng: -9.517401092833236
@@ -11,7 +11,7 @@ function initMap() {
 
     var locations = [
             {lat: 52.0595746, lng: -9.505322}, 
-            {lat: 52.20679235, lng: -10.03637366996086},
+            {lat: 52.1408381, lng: -10.2688831},
             {lat: 52.2692835, lng: -9.7054085},
             {lat: 52.4472298, lng: -9.4846811}, 
     ];
@@ -25,6 +25,42 @@ function initMap() {
 
     var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
+
+var locationsMap = [{
+    location: "Killarney",
+    description: "Placeholder",
+    LatLng: [{
+        lat: 52.0595746,
+        lng: -9.505322
+    }],
+
+},
+{
+    location: "Dingle",
+    description: "Placeholder",
+    LatLng: [{
+        lat: 52.1408381,
+        lng: -10.2688831
+    }],
+
+},
+    {
+    placeName: "Tralee",
+    description: "Placeholder",
+    LatLng: [{
+        lat: 52.2692835,
+        lng: -9.7054085
+    }],
+},
+{
+    placeName: "Kenmare",
+    description: "Placeholder",
+    LatLng: [{
+        lat: 52.4472298,
+        lng: -9.4846811
+    }]
+},
+];
 
 
 

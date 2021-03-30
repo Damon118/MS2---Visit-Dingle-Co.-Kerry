1453a37614cb84f76ec15332ecc4d4f089a82b17
Damon118 <damongaffney@rcsi.ie>
2021-03-30 00:57:43 +0000

    Add 4 markers with info to Google Maps API in thingstodo.html

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,4 +1,106 @@
+let map;
+let infoObject = [];
+let centerCords = {
+
+    lat: 52.14533445,
+    lng: -9.517401092833236
+};
+let markersOnMap = [{
+    placeName: "Killarney",
+    text: "Attractions include - The Lakes of Killarney, Ring of Kerry, Kerry Walking Trail and Killarney National Park",
+    LatLng: [{
+        lat: 52.0595746,
+        lng: -9.505322
+    }]
+},
+{
+    placeName: "Dingle",
+    text: "Attractions include - The Dingle Peninsula, world-renowned pub culture, the Gallarus Oratory and Kilmalkedar Church",
+    LatLng: [{
+        lat: 52.1408381,
+        lng: -10.2688831
+    }]
+},
+{
+    placeName: "Tralee",
+    text: "Attractions include - The Rose of Tralee Festival, Kerry County Museum and Tralee Town Park",
+    LatLng: [{
+        lat: 52.2692835,
+        lng: -9.7054085
+    }]
+},
+{
+    placeName: "Listowel",
+    text: "Attractions include - Listowel Castle, Kerry Writer's Museum and St. John's Art amd Heritage Centre",
+    LatLng: [{
+        lat: 52.4472298,
+        lng: -9.4846811
+    }]
+},
+];
+
+window.onload = function () {
+    initMap();
+};
+
+function addMarkerInfo() {
+    for (let i = 0; i < markersOnMap.length; i++) {
+        let contentString = '<div id="content"><h2>' + markersOnMap[i].placeName +
+            '</h2><p>' + markersOnMap[i].text + '</p></div>';
+
+        const marker = new google.maps.Marker({
+            position: markersOnMap[i].LatLng[0],
+            map: map
+        });
+
+        const infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 200
+        });
+
+        marker.addListener('click', function () {
+            closeOtherInfo();
+            infowindow.open(marker.get('map'), marker);
+            infoObject[0] = infowindow;
+        });
+    }
+}
+
+function closeOtherInfo() {
+    if (infoObject.length > 0) {
+        infoObject[0].set("marker", null);
+        infoObject[0].close();
+        infoObject.length = 0;
+    }
+}
+
 function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 7,
+        center: centerCords
+    });
+    addMarkerInfo();
+}
+
+document.getElementById("btn-killarney").addEventListener("click", initMapKillarney);
+
+function initMapKillarney() {
+    const killarney = { lat: 52.0595746, lng: -9.505322 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 10,
+        center: killarney,
+    });
+    const marker = new google.maps.Marker({
+        position: killarney,
+        map: map,
+    });
+}
+
+
+
+
+
+/* function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 7,
         center: {
@@ -60,7 +162,7 @@ var locationsMap = [{
         lng: -9.4846811
     }]
 },
-];
+]; */
 
 
 
